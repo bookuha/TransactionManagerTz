7abcc9674c3037419f92dfdd34db3a8cce420f5f
Bookuha <bookuha@gmail.com>
2024-08-12 14:44:38 +0300

    Fix Location conversion in Dapper Type Handler. Add Location Validation

--- src/TransactionManager.Data/Model/Location.cs
+++ src/TransactionManager.Data/Model/Location.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace TransactionManager.Data.Model;
 
 public record Location(double Latitude, double Longitude)
@@ -9,8 +11,15 @@ public record Location(double Latitude, double Longitude)
 
     public static Location FromString(string str)
     {
+        Regex locationRegex =
+            new(@"^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$");
+        
+        if (!locationRegex.IsMatch(str))
+        {
+            throw new ArgumentException("The location string is not in the correct format.");
+        }
+        
         var locationStringEntries = str.Split([", ", " "], StringSplitOptions.RemoveEmptyEntries);
-
         return new Location(Convert.ToDouble(locationStringEntries[0]), Convert.ToDouble(locationStringEntries[1]));
     }
 }
